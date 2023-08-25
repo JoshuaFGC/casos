41bccaf1f3f85082b47047ef1ee48c7cd07be9d1
JOSHUA FABIAN GUERRA CASTILLO <131022008+JoshuaFGC@users.noreply.github.com>
2023-08-25 12:05:34 -0600

    Archivos

---
+++ TDA.h
@@ -0,0 +1,11 @@
+/*Lista doble enlace
+metodos:
+agregar(string, pNoticia);
+buscar(string, pNoticia);
+eliminar(string, pNoticia);
+mostrar(string, pNoticas);
+Bool empty();
+int size();
+getNoticias();
+*/
+
---
+++ operaciones.h
@@ -0,0 +1,18 @@
+/*metodos:
+relevancia(string, pNoticias);
+    getNoticias();
+
+buscarNoticia(string, pPalabras);
+    /recibe input de una lista de palabras
+    /con un for se separan y se envian una por una a:
+    buscar(string, pPalabra);
+
+eliminarNoticia(string, pPalabras);
+    /recibe input de una lista de palabras
+    /con un for se separan y se envian una por una a:
+    buscar(string, pPalabra);
+
+relevancia(string, [+x, -x]);
+    buscar(string);
+    eliminar(string);
+*/
